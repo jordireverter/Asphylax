61a6d4e97412a6eda69a57227f709907e853d833
Jordi Reverter Palmada <u1972812@campus.udg.edu>
2026-07-20 01:01:27 +0200

    Logo canviat

--- client/app/controllers/history_controller.py
+++
@@ -1,9 +0,0 @@
-from app.services.agent_client import AgentClient
-
-
-class HistoryController:
-    def __init__(self):
-        self.agent_client = AgentClient()
-
-    def list_history(self) -> dict:
-        return self.agent_client.list_history()
